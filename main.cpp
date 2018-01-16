c3dcbd4f506fe539e7f0d19b9675c9a75633c54f
Andrei <andreialbu50@yahoo.com>
2018-01-16 17:48:59 +0200

    Change Monster name

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     Monster m_tank("Malphite", 0, 15, 7, 0, 0, 150);              //  - Player
     Monster m_doubleHit("MasterYi", 0, 5, 15, 1, 0, 100);    //
 
-    Monster mPC_adc("Ezreal", 0, 5, 20, 0, 0, 100);            //
+    Monster mPC_adc("Stefan", 0, 5, 20, 0, 0, 100);            //
     Monster mPC_tank("Rammus", 0, 15, 7, 0, 0, 150);     //   - PC
     Monster mPC_armpen("Darius", 1, 5, 16, 0, 0, 90);    //
 
